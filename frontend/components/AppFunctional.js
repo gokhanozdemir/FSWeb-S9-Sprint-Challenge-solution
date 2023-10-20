ed0fe5972f7a0b6bb8f477df38f0c3cb9f377b35
Gökhan <gn.ozdemir@gmail.com>
2023-10-20 16:41:34 +0300

    complete basic navigation

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from "react";
 
 // önerilen başlangıç stateleri
-const initialMessage = ''
-const initialEmail = ''
-const initialSteps = 0
-const initialIndex = 4 //  "B" nin bulunduğu indexi
+const initialMessage = "";
+const initialEmail = "";
+const initialSteps = 0;
+const initialIndex = 4; //  "B" nin bulunduğu indexi
 
 export default function AppFunctional(props) {
+  // State'lerinizi burada oluşturun
+  const [selected, setSelected] = useState(initialIndex);
   // AŞAĞIDAKİ HELPERLAR SADECE ÖNERİDİR.
   // Bunları silip kendi mantığınızla sıfırdan geliştirebilirsiniz.
 
@@ -23,12 +25,45 @@ export default function AppFunctional(props) {
 
   function reset() {
     // Tüm stateleri başlangıç ​​değerlerine sıfırlamak için bu helperı kullanın.
+    setSelected(initialIndex);
   }
 
   function sonrakiIndex(yon) {
     // Bu helper bir yön ("sol", "yukarı", vb.) alır ve "B" nin bir sonraki indeksinin ne olduğunu hesaplar.
     // Gridin kenarına ulaşıldığında başka gidecek yer olmadığı için,
     // şu anki indeksi değiştirmemeli.
+    console.log("sonrakiIndex", yon);
+    if (yon === "left") {
+      if (selected % 3 !== 0) {
+        setSelected(selected - 1);
+      } else {
+        console.log("sol kenar");
+      }
+    }
+
+    if (yon === "right") {
+      if (selected % 3 !== 2) {
+        setSelected(selected + 1);
+      } else {
+        console.log("sağ kenar");
+      }
+    }
+
+    if (yon === "up") {
+      if (selected > 2) {
+        setSelected(selected - 3);
+      } else {
+        console.log("üst kenar");
+      }
+    }
+
+    if (yon === "down") {
+      if (selected < 6) {
+        setSelected(selected + 3);
+      } else {
+        console.log("alt kenar");
+      }
+    }
   }
 
   function ilerle(evt) {
@@ -51,28 +86,40 @@ export default function AppFunctional(props) {
         <h3 id="steps">0 kere ilerlediniz</h3>
       </div>
       <div id="grid">
-        {
-          [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-              {idx === 4 ? 'B' : null}
-            </div>
-          ))
-        }
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((idx) => (
+          <div
+            key={idx}
+            className={`square${idx === selected ? " active" : ""}`}
+          >
+            {idx === selected ? "B" : null}
+            {idx}
+          </div>
+        ))}
       </div>
       <div className="info">
         <h3 id="message"></h3>
       </div>
       <div id="keypad">
-        <button id="left">SOL</button>
-        <button id="up">YUKARI</button>
-        <button id="right">SAĞ</button>
-        <button id="down">AŞAĞI</button>
-        <button id="reset">reset</button>
+        <button onClick={(e) => sonrakiIndex(e.target.id)} id="left">
+          SOL
+        </button>
+        <button onClick={(e) => sonrakiIndex(e.target.id)} id="up">
+          YUKARI
+        </button>
+        <button onClick={(e) => sonrakiIndex(e.target.id)} id="right">
+          SAĞ
+        </button>
+        <button onClick={(e) => sonrakiIndex(e.target.id)} id="down">
+          AŞAĞI
+        </button>
+        <button onClick={reset} id="reset">
+          reset
+        </button>
       </div>
       <form>
         <input id="email" type="email" placeholder="email girin"></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
-  )
+  );
 }
